3421fea15ab352edeaff1985522ba8bc22cb3bf5
N Gulshan Kumar <84889171+gulshan0201@users.noreply.github.com>
2026-01-16 08:01:20 +0000

    Done

--- AI-chat-bot/src/App.jsx
+++ AI-chat-bot/src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   
 
   return (
-    <>
-      <h1>AI-chat-bot</h1>
-    </>
+    <div className='grid grid-cols-5 h-screen text-center'>
+      <div className='col-span-1 bg-zinc-800'>
+       
+      </div>
+
+      <div className='col-span-4 p-10'>
+        <div className="container h-120">
+
+        </div>
+        
+        <div className='bg-zinc-800 w-1/2 p-1 pr-5. text-white m-auto rounded-4xl border border-zinc-400 flex h-16'>
+          <input type="text" className='w-full h-full p-3 outline-none' placeholder='Ask me anything' />
+          <button>ASK</button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
